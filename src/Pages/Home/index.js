373cfcbb7a061335ee5a6eac7065f072e3d8588e
Pedro Regenes <phregenes@gmail.com>
2020-11-29 08:19:12 -0300

    Feat: console

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -11,7 +11,7 @@ class Home extends Component {
     const response = await api.get('');
 
     this.setState({ episodes: response.data });
-    // console.log(response.data)
+      console.log(response.data)
   }
 
   render() {
